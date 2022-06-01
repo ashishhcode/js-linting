fdbbfe0fbc5a8863e2b84896ac85a10e2538059f
Jayant Pahuja <jayant.pahuja@hcode.tech>
2022-06-01 14:16:44 +0530

    testing linting

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [state, setState] = React.useState();
+  const unusedVar = '2';
+
+  React.useEffect(() => {
+    setState(state + 2);
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
